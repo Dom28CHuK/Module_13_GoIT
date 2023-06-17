0af088fc14721096ee1037c56fa798fefc189b5b
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-06-17 09:48:17 +0300

    start

---
+++ src/java/UserCrudApp.java
@@ -0,0 +1,10 @@
+import com.google.gson.Gson;
+
+public class UserCrudApp {
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/users";
+    private static final Gson gson = new Gson();
+
+    public static UserEntity createUser() {
+
+    }
+}
