a5d8acd5cfe88fb7703e0844fcdef87c4295ad51
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-06-17 12:24:47 +0300

    update

---
+++ src/java/httpurlconnection/Main.java
@@ -0,0 +1,8 @@
+package httpurlconnection;
+import static httpurlconnection.UserCrudApp.*;
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(createUser());
+        System.out.println(deleteUser(1));
+    }
+}
